928d5e33ad4047eefe01368b4f6156116483d6c6
rizkiwibu <98686896+rizkiwibu@users.noreply.github.com>
2023-05-09 08:32:43 +0700

    Update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -6,7 +6,7 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 ONLY WANT YOUR LOVE
 SUPPORT ME BY MAKING 69 MILLION FOLLOWERS ON MY INSTAGRAM`
 let img = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg'
-conn.sendHydrated2(m.chat, don, '▢ MAY LORD JAGANNATH BLESS YOUR DAY\n▢ ᴳᵁᴿᵁ  ┃ ᴮᴼᵀ\n▢ Subscribe  Youtube\nhttps://youtube.com/@Asliguru\n', img,'https://www.instagram.com/asli_guru69', 'Instagram', 'https://t.me/i_want_to_be_isekaied', 'TELEGRAM', [
+conn.sendHydrated2(m.chat, don, '▢ MAY LORD JAGANNATH BLESS YOUR DAY\n▢ ᴳᵁᴿᵁ  ┃ ᴮᴼᵀ\n▢ Subscribe  Youtube\nhttps://www.youtube.com/@RIZKIIRFAN\n', img,'https://www.instagram.com/ikykunnnn', 'Instagram', 'https://t.me/ikybot', 'TELEGRAM', [
     ], m)
 }
 
